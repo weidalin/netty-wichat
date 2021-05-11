87e88241e34b09a2210bdeb297e54c2c493c06c9
weidalin <752087756@qq.com>
2021-05-11 22:26:07 +0800

    chap2 netty hello world

---
+++ src/main/java/netty/HelloNetty.java
@@ -0,0 +1,35 @@
+package netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * 实现客户端发送请求，服务器会返回hello netty
+ */
+public class HelloNetty {
+    public static void main(String[] args) throws Exception {
+        //定义一对线程组
+        //主线程组 -- 不做事情
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        //从线程组 -- 做事情
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try{
+            //netty服务器的创建， ServerBootstrap是一个启动类
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup)   //设置主从线程组
+                    .channel(NioServerSocketChannel.class)  //设置nio的双向通道
+                    .childHandler(new HelloServerInitializer());                //子处理器，用于处理workergr
+            //启动server，并且设置8088为启动的端口号，同时启动方式为同步
+            ChannelFuture channelFuture = serverBootstrap.bind(8888).sync();
+            //监听关闭的channel,设置位同步方式
+            channelFuture.channel().closeFuture().sync();
+        } finally{
+            System.out.println("exit..........");
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
---
+++ src/main/java/netty/HelloServerInitializer.java
@@ -0,0 +1,25 @@
+package netty;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpServerCodec;
+
+/**
+ * 这是一个初始化其，channel注册之后，会执行里面的相应的初始化方法
+ */
+public class HelloServerInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel socketChannel) throws Exception {
+        //通过socketChannel去获得对应的管道
+        ChannelPipeline pipeline = socketChannel.pipeline();
+
+        //通过管道添加handler
+        //HttpServerCodec是由netty自己提供的助手类，也可以理解为拦截器
+        //当请求到服务端，我们需要做解码，相应到客户端做编码
+        pipeline.addLast("HttpServerCodedec", new HttpServerCodec());
+
+        //添加一个自定义的Handler
+        pipeline.addLast("customHandler", new CustomHandler());
+    }
+}
